fcf441d85509afd01c2520fad2eea63e8c1f1e09
William Munoz <william.munozr@gmail.com>
2022-10-05 22:00:33 -0500

    add array examples

--- arrays/main.go
+++ arrays/main.go
@@ -2,16 +2,54 @@ package main
 
 import "fmt"
 
-func main(){
-	var a[5]int
+type Currency int
+
+const (
+	USD Currency = iota
+	EUR
+	GBP
+	RMB
+)
+
+func main() {
+	var a [5]int
 	fmt.Println(a)
 	fmt.Printf("a length: %d\n", len(a))
 
-	b := [5]int{0,1,2,3,4}
+	b := [5]int{0, 1, 2, 3, 4}
 	fmt.Println(b)
 	fmt.Printf("b length: %d\n", len(b))
 
-	c := [5]int{0,1,2}
+	c := [5]int{0, 1, 2}
 	fmt.Println(c)
 	fmt.Printf("c length: %d\n", len(c))
+
+	arrayDeclaration()
+
+	symbol := [...]string{USD: "$", EUR: "€", GBP: "£", RMB: "¥"}
+	fmt.Println(RMB, symbol[RMB])
+}
+
+func arrayDeclaration() {
+	var a [3]int
+	fmt.Println(a[0])
+	fmt.Println(a[len(a)-1])
+
+	// print the indices and elements
+	for i, v := range a {
+		fmt.Printf("%d %d\n", i, v)
+	}
+
+	// print the elements only
+	for _, v := range a {
+		fmt.Printf("%d\n", v)
+	}
+
+	var q [3]int = [3]int{1, 2, 3}
+	var r [3]int = [3]int{1, 2}
+	fmt.Println(q[0])
+	fmt.Println(r[2])
+
+	p := [...]int{1, 2, 3}
+	fmt.Printf("%T\n", p)
 }
