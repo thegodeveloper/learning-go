232951d639d48f02b88c68dcf811ff62804b8753
William Munoz <william.munozr@gmail.com>
2022-10-13 12:36:22 -0500

    add more info to pointers

--- pointers/main.go
+++ pointers/main.go
@@ -2,12 +2,17 @@ package main
 
 import "fmt"
 
+// pointers in Go are types that store the address of the value, not the value.
+
+// you cannot store int in intPtr, you can only store the address of int
+var intPtr *int
+
 func a(i int) {
 	i = 0
 }
 
 func b(i *int) {
-	*i = 0
+	*i = 101
 }
 
 func main() {
@@ -20,5 +25,24 @@ func main() {
 	fmt.Println(x)
 
 	fmt.Println(&x)
+
+	// assign to intPtr the address of our x variable
+	intPtr = &x
+	fmt.Printf("x address: %v\n", &x)
+	fmt.Printf("intPtr address: %v\n", &intPtr)
+	fmt.Printf("intPtr value: %v\n", *intPtr)
+	fmt.Printf("x value: %v\n", x)
+	fmt.Printf("intPtr value: %v\n", *intPtr)
+	*intPtr = 80
+	fmt.Printf("x value: %v\n", x)
+
+	// pointers in functions
+	say := "hello"
+	changeValue(&say) // pass a pointer
+	fmt.Printf("now say variable has the value: %v\n", say)
 }
 
+func changeValue(word *string) {
+	// add "world" to the string pointed to by 'word'
+	*word += "world"
+}
