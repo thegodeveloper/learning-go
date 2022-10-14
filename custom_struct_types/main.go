849d3c339c2abdd5d0cb25ceb92f97cc85c0f714
William Munoz <william.munozr@gmail.com>
2022-10-13 22:39:04 -0500

    add custom struct types

---
+++ custom_struct_types/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+type Record struct {
+	Name string
+	Age int
+}
+
+// string returns a csv representing our record.
+func (r Record) String() string {
+	return fmt.Sprintf("%s,%d", r.Name, r.Age)
+}
+
+func main() {
+	david := Record{Name: "David Justice", Age: 28}
+	sarah := Record{Name: "Sarah Murphy", Age: 28}
+	fmt.Printf("%+v\n", david)
+	fmt.Printf("%+v\n", sarah)
+
+	john := Record{Name: "John Doak", Age: 100}
+	fmt.Println(john.String())
+
+	// changing a value
+	david.Age = 77
+	fmt.Println(david.String())
+}
+
