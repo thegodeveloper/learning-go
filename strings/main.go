4fda006a2596eb524ec62170843016ddddec0a2a
William Munoz <william.munozr@gmail.com>
2023-02-04 12:24:23 -0500

    working with strings

--- strings/main.go
+++ strings/main.go
@@ -12,4 +12,11 @@ func extractingSingleValue() {
 	var b byte = s[7]
 	fmt.Println("s:", s)
 	fmt.Println("b:", b)
+
+	s2 := s[4:7]
+	s3 := s[:5]
+	s4 := s[6:]
+	fmt.Println("s2:", s2)
+	fmt.Println("s3:", s3)
+	fmt.Println("s4:", s4)
 }
