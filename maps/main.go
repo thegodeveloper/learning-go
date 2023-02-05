2b85d757f2b7820118d3eef06d03287ab61ce40e
William Munoz <william.munozr@gmail.com>
2023-02-05 12:30:19 -0500

    working with maps

--- maps/main.go
+++ maps/main.go
@@ -45,4 +45,41 @@ func main() {
 	for key, val := range modelToMake {
 		fmt.Printf("car model %q has make %q\n", key, val)
 	}
+
+	fmt.Println()
+
+	mapsDeclarations()
+}
+
+func mapsDeclarations() {
+	// nilMap is declared to be a map with string keys and int values
+	// the zero value for a map is nil
+	// a nil map has a length of 0
+	// attempting to read a nil map always returns the zero value for the map's value type
+	fmt.Println("--- nil map ---")
+	var nilMap map[string]int
+	fmt.Println("nilMap:", nilMap)
+
+	// empty map literal
+	// is not the same as a nil map
+	// it has a length of zero
+	// you can read and write to a map assigned an empty map literal
+	fmt.Println("--- map literal ---")
+	totalWins := map[string]int{}
+	fmt.Println("totalWins:", totalWins)
+
+	fmt.Println("--- nonempty map literal ---")
+	teams := map[string][]string{
+		"Orcas":   {"Fred", "Ralph", "Bijou"},
+		"Lions":   {"Sarah", "Peter", "Billie"},
+		"Kittens": {"Waldo", "Raul", "Ze"},
+	}
+	fmt.Println("teams:", teams)
+	fmt.Println("teams.Orcas:", teams["Orcas"])
+	fmt.Println("teams.Lions.Peter:", teams["Lions"][1])
+
+	fmt.Println("--- map with a default size ---")
+	ages := make(map[int][]string, 10)
+	fmt.Println("ages:", ages)
+	fmt.Println("ages length:", len(ages))
 }
