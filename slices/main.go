b899989589c84f45de3e8f476b9757467ac6af00
William Munoz <william.munozr@gmail.com>
2023-02-22 18:25:28 -0500

    working with slices

--- slices/main.go
+++ slices/main.go
@@ -23,6 +23,7 @@ func main() {
 	copyingSliceToArray()
 	sliceWithSpecificValuesPerIndex()
 	sliceAppend()
+	sliceWithMake()
 }
 
 func slicingSlices() {
@@ -132,4 +133,16 @@ func sliceWithSpecificValuesPerIndex() {
 	x := []int{1, 5: 6, 10: 100, 15}
 	println("--- slice with specific values per index ---")
 	println("x values: ", x)
+}
+
+func sliceWithMake() {
+	x := make([]int, 7)
+	fmt.Println("--- Slice with make ---")
+	fmt.Println("x value:", x)
+
+	fmt.Println("--- Setting the initial capacity with make ---")
+	y := make([]int, 7, 10)
+	fmt.Println("y values:", y)
+	fmt.Println("y length:", len(y))
+	fmt.Println("y capacity:", cap(y))
 }
