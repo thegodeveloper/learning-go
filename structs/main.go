b9f13d4339f210a5e6d829bab41247c400ad953f
William Munoz <william.munozr@gmail.com>
2023-02-22 19:40:05 -0500

    working with structs

---
+++ structs/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+type person struct {
+	name string
+	age  int
+	pet  string
+}
+
+func main() {
+	structDeclaration()
+	anonymousStruct()
+}
+
+func structDeclaration() {
+	fmt.Println("--- Struct Declaration ---")
+	beth := person{
+		age:  37,
+		pet:  "dog",
+		name: "Beth",
+	}
+	fmt.Println(beth)
+
+	beth.name = "beth"
+	fmt.Println("Name:", strings.ToTitle(beth.name))
+	fmt.Println("Age:", beth.age)
+	fmt.Println("Pet:", beth.pet)
+}
+
+func anonymousStruct() {
+	fmt.Println("--- Anonymous Struct ---")
+	pet := struct {
+		name string
+		kind string
+	}{
+		name: "Fido",
+		kind: "dog",
+	}
+
+	fmt.Println("Pet Value:", pet)
+}
