9186082f39260b9556d455310e628b3d44b9af40
william <william.munozr@gmail.com>
2023-03-02 14:55:06 -0500

    add for in strings

--- for_loops/for_string.go
+++ for_loops/for_string.go
@@ -3,6 +3,7 @@ package main
 import "fmt"
 
 func forString() {
+	fmt.Println("--- for in string ---")
 	samples := []string{"hello", "world!"}
 	for _, sample := range samples {
 		for i, r := range sample {
