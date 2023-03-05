9b75f0d8db1fe84a6312772cc8a65b1cdb087cd5
william <william.munozr@gmail.com>
2023-03-05 18:18:19 -0500

    add complex number basics

---
+++ complex/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a complex128 = complex(6, 2)
+	var b complex64 = complex(9, 2)
+	fmt.Println(a)
+	fmt.Println(b)
+
+	fmt.Printf("The type of a is %T and the type of b is %T", a, b)
+}
