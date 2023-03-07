cf04b507e37eef6ab75c140f5760cf295efae844
william <william.munozr@gmail.com>
2023-03-07 18:57:32 -0500

    add functional options pattern in go

---
+++ functional_options_pattern/main.go
@@ -0,0 +1,72 @@
+package main
+
+import "fmt"
+
+type Pizza struct {
+	pepper         bool
+	salt           bool
+	cheese         bool
+	extraCheese    bool
+	chickenTopping bool
+	paneerTopping  bool
+}
+
+// Options option type which return a function
+type Options func(config *Pizza)
+
+func BuildPizza(options ...Options) *Pizza {
+	p := &Pizza{
+		salt:   true,
+		cheese: true,
+	}
+
+	for _, opt := range options {
+		if opt != nil {
+			opt(p)
+		}
+	}
+
+	return p
+}
+
+func WIthPepper() func(config *Pizza) {
+	return func(config *Pizza) {
+		config.pepper = true
+	}
+}
+
+func WithSalt() func(config *Pizza) {
+	return func(config *Pizza) {
+		config.salt = true
+	}
+}
+
+func WithCheese() func(config *Pizza) {
+	return func(config *Pizza) {
+		config.cheese = true
+	}
+}
+
+func WithExtraCheese() func(config *Pizza) {
+	return func(config *Pizza) {
+		config.extraCheese = true
+	}
+}
+
+func WithChickenTopping() func(config *Pizza) {
+	return func(config *Pizza) {
+		config.chickenTopping = true
+	}
+}
+
+func WithPaneerTopping() func(config *Pizza) {
+	return func(config *Pizza) {
+		config.paneerTopping = true
+	}
+}
+
+func main() {
+	coolPizza := BuildPizza(WIthPepper(), WithExtraCheese(), WithChickenTopping())
+
+	fmt.Printf("eating delicious Pizza: %v", coolPizza)
+}
---
+++ functional_options_pattern/readme.md
@@ -0,0 +1,3 @@
+# Functional Options Patterns in Go
+
+- https://medium.com/@saiteja180/functional-options-pattern-in-go-b9ecb2a043db
