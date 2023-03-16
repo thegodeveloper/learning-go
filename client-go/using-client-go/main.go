9f9bc1f731781dd2cbfba729451ad1d59f4f062b
william <william.munozr@gmail.com>
2023-03-16 18:34:00 -0500

    add client-go and apimachinery

--- client-go/using-client-go/main.go
+++ client-go/using-client-go/main.go
@@ -2,16 +2,29 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	metav1 "k8s.io/apimachinery/api/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
+	"log"
 )
 
 func main() {
-	kubeconfig = flag.String("kubeconfig", "~/.kube/config", "kubeconfig file")
+	kubeconfig := flag.String("kubeconfig", "~/.kube/config", "kubeconfig file")
 	flag.Parse()
 	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	if err != nil {
+		log.Fatal(err)
+	}
 	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	pod, err := clientset.CoreV1().Pods("book").Get("example", metav1.GetOptions{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("value of pod: %v\n", pod)
 }
