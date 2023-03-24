ae4dca4be3744ef89b2bc7b54ddd532c3bd1ef6b
william <william.munozr@gmail.com>
2023-03-24 12:26:51 -0500

    add errors and values

---
+++ errors/errors_and_values/main.go
@@ -0,0 +1,54 @@
+package main
+
+import "fmt"
+
+type Status int
+
+const (
+	InvalidLogin Status = iota + 1
+	NotFound
+)
+
+type StatusErr struct {
+	Status  Status
+	Message string
+}
+
+func (se StatusErr) Error() string {
+	return se.Message
+}
+
+func main() {
+	data, err := LoginAndGetData("uid", "pwd", "file")
+	if err != nil {
+		fmt.Println("error opening a file")
+		return
+	}
+	fmt.Println(data)
+}
+
+func LoginAndGetData(uid, pwd, file string) ([]byte, error) {
+	err := login(uid, pwd)
+	if err != nil {
+		return nil, StatusErr{
+			Status:  InvalidLogin,
+			Message: fmt.Sprintf("invalid credentials for user %s", uid),
+		}
+	}
+	data, err := getData(file)
+	if err != nil {
+		return nil, StatusErr{
+			Status:  NotFound,
+			Message: fmt.Sprintf("file %s not found", file),
+		}
+	}
+	return data, nil
+}
+
+func login(uid, pwd string) error {
+	return nil
+}
+
+func getData(file string) ([]byte, error) {
+	return nil, nil
+}
