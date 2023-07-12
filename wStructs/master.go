aeef662ecc2cff1817be826ef026ba1f168ec490
william <william@godeveloper.io>
2023-07-11 20:21:41 -0500

    more about structs

--- wStructs/master.go
+++ wStructs/master.go
@@ -17,6 +17,12 @@ func Master(show bool) {
 			13000,
 			123)
 		fmt.Println(bill)
+
+		a := []int{777, 7, 77, 77777, 7777}
+		fmt.Printf("Initial value of a: %v\n", a)
+		Sort(a)
+		fmt.Printf("Final value of a: %v\n", a)
+
 	}
 }
 
@@ -40,4 +46,42 @@ type Employee struct {
 	Position  string
 	Salary    int
 	ManagerID int
+}
+
+type tree struct {
+	value       int
+	left, right *tree
+}
+
+// Sort sorts values in place.
+func Sort(values []int) {
+	var root *tree
+	for _, v := range values {
+		root = add(root, v)
+	}
+	appendValues(values[:0], root)
+}
+
+func appendValues(values []int, t *tree) []int {
+	if t != nil {
+		values = appendValues(values, t.left)
+		values = append(values, t.value)
+		values = appendValues(values, t.right)
+	}
+	return values
+}
+
+func add(t *tree, value int) *tree {
+	if t == nil {
+		// Equivalent to return &tree{value: value}
+		t = new(tree)
+		t.value = value
+		return t
+	}
+	if value < t.value {
+		t.left = add(t.left, value)
+	} else {
+		t.right = add(t.right, value)
+	}
+	return t
 }
