b933bd7f046f792b8b1da2f5dac0954c7cc7cf79
William Munoz <william@rustdeveloper.io>
2024-02-29 17:31:15 -0500

    add integer type conversions

--- internal/wtypeconversions/master.go
+++ internal/wtypeconversions/master.go
@@ -7,6 +7,7 @@ func Master(show bool) {
 		fmt.Println("-- Type Conversions")
 
 		definition(false)
+		integerTypeConversion(false)
 	}
 }
 
@@ -21,5 +22,16 @@ func definition(show bool) {
 
 		fmt.Println("sum1 = ", sum1)
 		fmt.Println("sum2 = ", sum2)
+	}
+}
+
+func integerTypeConversion(show bool) {
+	if show {
+		var x int = 10
+		var b byte = 100
+		var sum3 int = x + int(b)
+		var sum4 byte = byte(x) + b
+		fmt.Println("sum3 = ", sum3)
+		fmt.Println("sum4 = ", sum4)
 	}
 }
