23576bfc28b34182bee82ecb866ec1742c599c91
William Munoz <william@rustdeveloper.io>
2024-03-01 18:17:25 -0500

    working with append and slices

--- internal/slices/sliceAppend.go
+++ internal/slices/sliceAppend.go
@@ -6,17 +6,32 @@ func sliceAppend(show bool) {
 	if show {
 		fmt.Println("--- Slice Append")
 
+		// This is a nil slice
 		var x []int
 		fmt.Println("Initial Slice Length:", len(x))
 		fmt.Println("Initial Slice Capacity:", cap(x))
+
 		x = append(x, 10)
 		fmt.Println("Slice Append, value of x:", x)
 		fmt.Println("Slice Length:", len(x))
 		fmt.Println("Slice Capacity:", cap(x))
+
 		x = append(x, 20)
 		fmt.Println("Slice Capacity:", cap(x))
+
 		x = []int{10, 20, 30}
 		fmt.Println("Current x values:", x)
 		fmt.Println("New slice capacity:", cap(x))
+
+		// Append more than one value
+		x = append(x, 40, 50, 60, 70)
+		fmt.Println("New x values:", x)
+		fmt.Println("New x capacity:", cap(x))
+
+		// Append a slice into another
+		y := []int{80, 90, 100}
+		x = append(x, y...)
+		fmt.Println("New x values:", x)
+		fmt.Println("New x capacity:", cap(x))
 	}
 }
