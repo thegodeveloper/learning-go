8470018437ffcdb4cb96680769e46ab2ce15f772
William Munoz <william@rustdeveloper.io>
2024-03-02 08:40:25 -0500

    refactoring enums using constants

--- internal/enums/definition.go
+++ internal/enums/definition.go
@@ -8,7 +8,7 @@ const (
 	Monday DayOfTheWeek = iota
 	Tuesday
 	Wednesday
-	Thurday
+	Thursday
 	Friday
 	Saturday
 	Sunday
@@ -17,7 +17,11 @@ const (
 func Master(show bool) {
 	if show {
 		fmt.Printf("Monday is %d\n", Monday)
+		fmt.Printf("Tuesday is %d\n", Tuesday)
 		fmt.Printf("Wednesday is %d\n", Wednesday)
+		fmt.Printf("Thursday is %d\n", Thursday)
 		fmt.Printf("Friday is %d\n", Friday)
+		fmt.Printf("Saturday is %d\n", Saturday)
+		fmt.Printf("Sunday is %d\n", Sunday)
 	}
 }
