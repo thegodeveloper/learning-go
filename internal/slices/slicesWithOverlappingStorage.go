7f89bc537bc9db2bb655631acc89183edbfd19d6
William Munoz <william@rustdeveloper.io>
2024-08-28 00:56:21 -0500

    add slices with overlapping storage

--- internal/slices/slicesWithOverlappingStorage.go
+++ internal/slices/slicesWithOverlappingStorage.go
@@ -6,6 +6,7 @@ func slicesWithOverlappingStorage(show bool) {
 	if show {
 		fmt.Println("Slices with Overlapping Storage")
 
+		// changing x modified both y and z, while changes to y and z modified x.
 		x := []string{"A", "B", "C", "D"}
 		y := x[:2]
 		z := x[1:]
