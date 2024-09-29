11e11aba6c5e55264361803adaecee5c62008d61
William Munoz <william@rustdeveloper.io>
2024-09-29 14:58:57 -0500

    add anonymous function option 2

--- internal/functions/anonymous_functions.go
+++ internal/functions/anonymous_functions.go
@@ -4,6 +4,13 @@ import "fmt"
 
 func mainAnonymousFunctions(show bool) {
 	if show {
+		anonymousFunctionOption1(false)
+		anonymousFunctionOption2(false)
+	}
+}
+
+func anonymousFunctionOption1(show bool) {
+	if show {
 		fmt.Println("--- Anonymous Functions ---")
 
 		f := func(j int) {
@@ -12,6 +19,16 @@ func mainAnonymousFunctions(show bool) {
 
 		for i := 0; i < 5; i++ {
 			f(i)
+		}
+	}
+}
+
+func anonymousFunctionOption2(show bool) {
+	if show {
+		for i := 0; i < 5; i++ {
+			func(j int) {
+				fmt.Println("printing", j, "from inside of an anonymous function")
+			}(i)
 		}
 	}
 }
