8b7c28f9b2a7bf4354412e7c0c711c255a1de816
William Munoz <william@rustdeveloper.io>
2024-10-09 10:02:10 -0500

    add calculator test cases

--- internal/wcalculator/calculator_test.go
+++ internal/wcalculator/calculator_test.go
@@ -6,23 +6,69 @@ import (
 
 func TestAdd(t *testing.T) {
 	t.Parallel()
-	var want float64 = 4
 
-	got := Add(2, 2)
+	type testCase struct {
+		a, b float64
+		want float64
+	}
 
-	if want != got {
-		t.Errorf("want %f, got %f", want, got)
+	testCases := []testCase{
+		{a: 2, b: 2, want: 4},
+		{a: 1, b: 1, want: 2},
+		{a: 7, b: 0, want: 7},
+	}
+
+	for _, tc := range testCases {
+		got := Add(tc.a, tc.b)
+
+		if tc.want != got {
+			t.Errorf("Add(%f, %f): want %f, got %f", tc.a, tc.b, tc.want, got)
+		}
 	}
 }
 
 func TestSubtract(t *testing.T) {
 	t.Parallel()
+	type testCase struct {
+		a, b float64
+		want float64
+	}
 
-	var want float64 = 2
+	testCases := []testCase{
+		{a: 4, b: 2, want: 2},
+		{a: 1, b: 1, want: 0},
+		{a: 7, b: -14, want: 21},
+		{a: 0, b: 7, want: -7},
+	}
 
-	got := Subtract(4, 2)
+	for _, tc := range testCases {
+		got := Subtract(tc.a, tc.b)
 
-	if want != got {
-		t.Errorf("want %f, got %f", want, got)
+		if tc.want != got {
+			t.Errorf("Subtract(%f, %f): want %f, got %f", tc.a, tc.b, tc.want, got)
+		}
+	}
+}
+
+func TestMultiply(t *testing.T) {
+	t.Parallel()
+
+	type testCase struct {
+		a, b float64
+		want float64
+	}
+
+	testCases := []testCase{
+		{a: 4, b: 2, want: 8},
+		{a: 1, b: 1, want: 1},
+		{a: 7, b: 11, want: 77},
+	}
+
+	for _, tc := range testCases {
+		got := Multiply(tc.a, tc.b)
+
+		if tc.want != got {
+			t.Errorf("Multiply(%f, %f): want %f, got %f", tc.a, tc.b, tc.want, got)
+		}
 	}
 }
