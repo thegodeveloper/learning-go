8534c586eb4f0ced3bb0fbc253eb69eaf0d0498e
William Munoz <william@rustdeveloper.io>
2025-01-18 15:26:05 -0500

    add reading from command line

--- internal/input/definition.go
+++ internal/input/definition.go
@@ -1,12 +1,17 @@
 package input
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
 
 func Master(show bool) {
 	if show {
 		fmt.Println("Input Definition")
 
 		readingFromStandardInput(false)
+		readingArgsFromCommandLine(false)
 	}
 }
 
@@ -22,5 +27,42 @@ func readingFromStandardInput(show bool) {
 		} else {
 			fmt.Println("Your name is:", name)
 		}
+	}
+}
+
+func readingArgsFromCommandLine(show bool) {
+	if show {
+		fmt.Println("Reading args from command line")
+
+		arguments := os.Args
+		if len(arguments) == 1 {
+			fmt.Println("Need one or more arguments!")
+			return
+		}
+
+		var minv, maxv float64
+		var initialized int
+
+		for i := 1; i < len(arguments); i++ {
+			n, err := strconv.ParseFloat(arguments[i], 64)
+			if err != nil {
+				continue
+			}
+			if initialized == 0 {
+				minv = n
+				maxv = n
+				initialized = 1
+				continue
+			}
+			if n < minv {
+				minv = n
+			}
+			if n > maxv {
+				maxv = n
+			}
+		}
+
+		fmt.Println("Min:", minv)
+		fmt.Println("Max:", maxv)
 	}
 }
