087982922272b09e36c18c708402adc3e540b047
William Munoz <william@rustdeveloper.io>
2025-01-18 15:53:40 -0500

    add divide function

--- internal/wcalculator/calculator.go
+++ internal/wcalculator/calculator.go
@@ -1,6 +1,8 @@
 // Package wCalculator does simple calculations.
 package wcalculator
 
+import "fmt"
+
 // Add takes two numbers and returns the result of adding them together.
 func Add(a, b float64) float64 {
 	return a + b
@@ -12,4 +14,11 @@ func Subtract(a, b float64) float64 {
 
 func Multiply(a, b float64) float64 {
 	return a * b
+}
+
+func Divide(a, b float64) (float64, error) {
+	if b == 0 {
+		return 0, fmt.Errorf("divide by zero")
+	}
+	return a / b, nil
 }
