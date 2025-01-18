770e651d5df311ee4b66cc892a879e71b0f9f922
William Munoz <william@rustdeveloper.io>
2025-01-18 16:20:29 -0500

    add differentiate between input types

--- internal/input/definition.go
+++ internal/input/definition.go
@@ -12,6 +12,7 @@ func Master(show bool) {
 
 		readingFromStandardInput(false)
 		readingArgsFromCommandLine(false)
+		differentiateBetweenInputTypes(false)
 	}
 }
 
@@ -64,5 +65,43 @@ func readingArgsFromCommandLine(show bool) {
 
 		fmt.Println("Min:", minv)
 		fmt.Println("Max:", maxv)
+	}
+}
+
+func differentiateBetweenInputTypes(show bool) {
+	if show {
+		fmt.Println("Differentiate between input types")
+
+		arguments := os.Args
+		if len(arguments) == 1 {
+			fmt.Println("Not enough arguments!")
+			return
+		}
+
+		var total, nInts, nFloats int
+		invalid := make([]string, 0)
+		for _, k := range arguments[1:] {
+			_, err := strconv.Atoi(k)
+			if err == nil {
+				total++
+				nInts++
+				continue
+			}
+			_, err = strconv.ParseFloat(k, 64)
+			if err == nil {
+				total++
+				nFloats++
+				continue
+			}
+			invalid = append(invalid, k)
+		}
+
+		fmt.Println("#read:", total, "#ints:", nInts, "#floats:", nFloats)
+		if len(invalid) > 0 {
+			fmt.Println("Too much invalid input:", len(invalid))
+			for _, s := range invalid {
+				fmt.Println(s)
+			}
+		}
 	}
 }
