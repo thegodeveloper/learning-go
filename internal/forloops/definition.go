7ca247fde417151d98cdc02d3bb2fec5787ae4a8
William Munoz <william@rustdeveloper.io>
2025-01-18 13:05:09 -0500

    add for with range loop

--- internal/forloops/definition.go
+++ internal/forloops/definition.go
@@ -38,9 +38,15 @@ func definition(show bool) {
 
 		// idiomatic for loop
 		i := 0
-		for ok := true; ok; ok = (i != 10) {
+		for ok := true; ok; ok = i != 10 {
 			fmt.Print(i*i, " ")
 			i++
+		}
+
+		// for with range
+		aSlice := []int{-1, 2, 1, -1, 2, -2}
+		for i, v := range aSlice {
+			fmt.Println("index:", i, "value:", v)
 		}
 	}
 }
