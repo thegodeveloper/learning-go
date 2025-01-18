bcc01cbdfa55b46e1be612e0e908e778c979e3b3
William Munoz <william@rustdeveloper.io>
2025-01-18 12:57:17 -0500

    add comments to for loop definition

--- internal/forloops/definition.go
+++ internal/forloops/definition.go
@@ -8,16 +8,19 @@ func definition(show bool) {
 		x := 5
 		counter := x
 
+		// for with a condition
 		for counter > 0 {
 			fmt.Println(counter)
 			counter--
 		}
 
+		// standard for loop
 		for i := 0; i < x; i++ {
 			fmt.Print(i)
 		}
 		fmt.Println()
 
+		// for with continue and break
 		for {
 			if x%2 != 0 {
 				fmt.Printf("%d is odd\n", x)
@@ -27,6 +30,7 @@ func definition(show bool) {
 			break
 		}
 
+		// infinite for loop with break
 		for {
 			fmt.Println("Never stop")
 			break
