7b6ba19d081667958f9f5e7924cee98b4f94848b
William Munoz <william@rustdeveloper.io>
2025-01-22 11:39:32 -0500

    working with structs

--- internal/structs/records.go
+++ internal/structs/records.go
@@ -11,6 +11,8 @@ func record(show bool) {
 	if show {
 		record := createPersona("Bill", 50)
 
+		fmt.Println(record.String())
+
 		fmt.Println("Name:", record.Name)
 		fmt.Println("Age:", record.Age)
 	}
@@ -18,4 +20,10 @@ func record(show bool) {
 
 func createPersona(name string, age int) Persona {
 	return Persona{Name: name, Age: age}
+}
+
+// String returns a csv representating our record.
+// This function receive a copy of the struct
+func (r Persona) String() string {
+	return fmt.Sprintf("%s,%d", r.Name, r.Age)
 }
