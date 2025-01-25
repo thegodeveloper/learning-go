177e383b30c18719855595a80a142e0f32eb2af6
William Munoz <william@rustdeveloper.io>
2025-01-24 19:12:49 -0500

    add constants

--- internal/constants/definition.go
+++ internal/constants/definition.go
@@ -1,10 +1,25 @@
 package constants
 
+import "fmt"
+
 const monthsInYear = 12
+const str = "hello world"
+const num = 3
+const num64 int64 = 3
+
+type specialStr string
+
+func printSpecial(str specialStr) {
+	fmt.Println(string(str))
+}
 
 func Master(show bool) {
 	if show {
 		println("Constants")
 		println("Constant value: ", monthsInYear)
+
+		const constHelloWorld = "hello world"
+		printSpecial(constHelloWorld)
+		printSpecial("hello world")
 	}
 }
