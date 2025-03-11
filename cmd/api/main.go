21a2d1ab3525524a8b1f2650005b87a16409ed3d
William Munoz <william@rustdeveloper.io>
2025-03-11 15:06:21 -0500

    starting professional go developer

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -32,6 +32,7 @@ import (
 	"github.com/thegodeveloper/learning-go/internal/passingarguments"
 	"github.com/thegodeveloper/learning-go/internal/pointers"
 	"github.com/thegodeveloper/learning-go/internal/printstars"
+	"github.com/thegodeveloper/learning-go/internal/random_message"
 	"github.com/thegodeveloper/learning-go/internal/runes"
 	"github.com/thegodeveloper/learning-go/internal/sha256"
 	"github.com/thegodeveloper/learning-go/internal/slices"
@@ -140,4 +141,5 @@ func main() {
 	logfatal.Master(false)
 	logging.Master(false, true, false)
 	statistics.Master(false)
+	randommessage.Index(false)
 }
---
+++ internal/random_message/index.go
@@ -0,0 +1,31 @@
+package randommessage
+
+import (
+	"errors"
+	"fmt"
+	"log"
+	"math/rand"
+	"strconv"
+	"time"
+)
+
+var helloList = []string{
+	"Hello, world", "Καλημέρα κόσμε", "سلام دنیا", "Привет, мир",
+}
+
+func Index(show bool) {
+	rand.NewSource(time.Now().UnixNano())
+	index := rand.Intn(len(helloList))
+	msg, err := hello(index)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(msg)
+}
+
+func hello(index int) (string, error) {
+	if index < 0 || index > len(helloList)-1 {
+		return "", errors.New("out of range:" + strconv.Itoa(index))
+	}
+	return helloList[index], nil
+}
