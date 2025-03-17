3d95b4109a00c7518e5670943462cc765382aef7
William Munoz <william@rustdeveloper.io>
2025-03-17 15:23:55 -0500

    add books learning go chapter5 ex2

--- internal/books/learning_go/chapter5/chapter5.go
+++ internal/books/learning_go/chapter5/chapter5.go
@@ -3,5 +3,6 @@ package chapter5
 func Index(show bool) {
 	if show {
 		ex1(false)
+		ex2(false)
 	}
 }
---
+++ internal/books/learning_go/chapter5/ex2.go
@@ -0,0 +1,42 @@
+package chapter5
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
+func fileLen(fileName string) (int, error) {
+	f, err := os.Open(fileName)
+	if err != nil {
+		return 0, err
+	}
+	defer f.Close()
+	data := make([]byte, 2048)
+	total := 0
+	for {
+		count, err := f.Read(data)
+		total += count
+		if err != nil {
+			if err != io.EOF {
+				return 0, err
+			}
+			break
+		}
+	}
+	return total, nil
+}
+
+func ex2(show bool) {
+	if show {
+		if len(os.Args) < 2 {
+			return
+		}
+		count, err := fileLen(os.Args[1])
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(count)
+	}
+}
