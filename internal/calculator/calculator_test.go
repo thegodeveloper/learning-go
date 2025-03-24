a06b6fc93c8004702eaee88ad660d1666f978361
William Munoz <william@rustdeveloper.io>
2025-03-24 12:42:55 -0500

    add subtract calculator function

--- internal/calculator/calculator_test.go
+++ internal/calculator/calculator_test.go
@@ -9,9 +9,21 @@ import (
 func TestAdd(t *testing.T) {
 	t.Parallel()
 
-	var want float64 = 49
+	var want float64 = 14
 
 	got := calculator.Add(7, 7)
+
+	if want != got {
+		t.Errorf("want %f, got %f", want, got)
+	}
+}
+
+func TestSubtract(t *testing.T) {
+	t.Parallel()
+
+	var want float64 = 7
+
+	got := calculator.Subtract(14, 7)
 
 	if want != got {
 		t.Errorf("want %f, got %f", want, got)
--- internal/calculator/index.go
+++ internal/calculator/index.go
@@ -8,6 +8,11 @@ func Add(a, b float64) float64 {
 	return a + b
 }
 
+// Subtract takes two numbers a and b, and returns the result of subtracting b from a
+func Subtract(a, b float64) float64 {
+	return a - b
+}
+
 func Index(show bool) {
 	if show {
 		fmt.Println("Calculator App")
