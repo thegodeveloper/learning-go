e7128fba80d41395429b0a6b045f963fc18e56e5
William Munoz <william@rustdeveloper.io>
2025-03-28 08:48:01 -0500

    document the interface declaration

--- internal/interfaces/definition.go
+++ internal/interfaces/definition.go
@@ -2,7 +2,7 @@ package interfaces
 
 import "fmt"
 
-// list the methods that must be implemented by a concrete type to meet the interface
+// List the methods that must be implemented by a concrete type to meet the interface
 type Stringer interface {
 	String() string
 }
@@ -11,6 +11,10 @@ type Persona struct {
 	First, Last string
 }
 
+// A concrete type does not declare that it implements an interface.
+// If the method set for a concrete type contains all the methods in the method set for the interface,
+// the concrete type implements the interface.
+// It is implemented implicity.
 func (p Persona) String() string {
 	return fmt.Sprintf("%s %s", p.First, p.Last)
 }
