e6d21bbb8de9d0330243dae7439cf210eaf51b18
William Munoz <william@zen.homelabwm.net>
2025-09-20 09:27:38 -0500

    add comments to pointer receiver

--- internal/structs/pointer_receiver.go
+++ internal/structs/pointer_receiver.go
@@ -10,12 +10,16 @@ type Counter struct {
 	lastUpdated time.Time
 }
 
+// the receiver is being modified by the method
+// so we are using a pointer receiver
 func (c *Counter) Increment() {
 	c.total++
 	c.lastUpdated = time.Now()
 }
 
-func (c Counter) String() string {
+// is good practice in the methods definition to be consistent if one is a pointer receiver
+// the others methods should be pointer receivers even if they don't change the receiver
+func (c *Counter) String() string {
 	return fmt.Sprintf("total: %d, last updated: %v", c.total, c.lastUpdated)
 }
 
