cca6c2117c1fbae9a0439cdb6c858d7791562ce3
William Munoz <william@rustdeveloper.rs>
2025-10-05 21:55:06 -0500

    create a constant in basics package

--- internal/basics/net.go
+++ internal/basics/net.go
@@ -6,11 +6,13 @@ import (
 	"net/http"
 )
 
+const staticUrl = "http://www.google.com"
+
 func net(show bool) {
 	if show {
 		fmt.Println("Go Standard Library")
 
-		resp, err := http.Get("http://www.google.com")
+		resp, err := http.Get(staticUrl)
 		if err != nil {
 			fmt.Println("Error:", err)
 			return
