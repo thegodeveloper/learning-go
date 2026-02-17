683f3cd8d73e4237b4edf71e5c015cfee4765b71
William Munoz <william@rustdeveloper.rs>
2026-02-16 19:48:05 -0500

    add short version of the range

--- internal/arrays/arrayStrings.go
+++ internal/arrays/arrayStrings.go
@@ -6,8 +6,8 @@ func ArrayStrings(show bool) {
 	if show {
 		var germanNumbers = [10]string{"eins", "zwei", "drei", "vier", "funf", "sechs", "sieben", "acth", "neun", "zehn"}
 
-		for num, value := range germanNumbers {
-			fmt.Printf("number %d in German is %s\n ", num+1, value)
+		for i, v := range germanNumbers {
+			fmt.Printf("number %d in German is %s\n ", i+1, v)
 		}
 	}
 }
