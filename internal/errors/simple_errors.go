fdde9d3769bd47a6bd58323d8a015566d027eb7e
William Munoz <william@rustdeveloper.io>
2026-06-10 10:00:55 -0500

    add ways to add errors

--- internal/errors/simple_errors.go
+++ internal/errors/simple_errors.go
@@ -12,16 +12,33 @@ func doubleEven(i int) (int, error) {
 	return i * 2, nil
 }
 
+func doubleEvenWithErrorf(i int) (int, error) {
+	if i%2 == 0 {
+		return 0, fmt.Errorf("%d isn't an even number", i)
+	}
+	return i * 2, nil
+}
+
 // SimpleErrors demonstrates simple error creation with errors.New
 func SimpleErrors(show bool) {
 	if !show {
 		return
 	}
 	simpleErrors()
+	simpleErrorsWithErrorf()
 }
 
 func simpleErrors() {
 	evenNumber, err := doubleEven(7)
+	if err != nil {
+		fmt.Println("Err:", err)
+		return
+	}
+	fmt.Printf("%d is even", evenNumber)
+}
+
+func simpleErrorsWithErrorf() {
+	evenNumber, err := doubleEvenWithErrorf(7)
 	if err != nil {
 		fmt.Println("Err:", err)
 		return
