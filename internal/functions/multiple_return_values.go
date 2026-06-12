63fe41382395adfce7717da843a9548fc86a8041
William Munoz <william@rustdeveloper.io>
2026-06-12 16:09:42 -0500

    adjusting multiple return values

--- internal/functions/multiple_return_values.go
+++ internal/functions/multiple_return_values.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 )
 
-func divAndRemainder(numerator int, denominator int) (int, int, error) {
+func divAndRemainder(numerator, denominator int) (int, int, error) {
 	if denominator == 0 {
 		return 0, 0, errors.New("cannot divide by zero")
 	}
