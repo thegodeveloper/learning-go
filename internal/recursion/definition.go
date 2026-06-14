d1a770abe5f0b69fc56728fa5e28239df5bcc1b8
William Munoz <william@rustdeveloper.io>
2026-06-13 23:10:34 -0500

    add recursion definition

--- internal/recursion/definition.go
+++ internal/recursion/definition.go
@@ -8,6 +8,10 @@ func Definition(show bool) {
 
 		fmt.Println("factorial(5):", factorial(5))
 		fmt.Println("factorial(10):", factorial(10))
+
+		fmt.Println("sumOfDigis(7):", sumOfDigits(7))
+		fmt.Println("sumOfDigis(12):", sumOfDigits(12))
+		fmt.Println("sumOfDigis(12345):", sumOfDigits(12345))
 	}
 }
 
@@ -18,4 +22,13 @@ func factorial(n int) int {
 	}
 	// n * (n - 1) * (n - 2) * factorial (n - 3)... until factorial(0) = 1
 	return n * factorial(n-1)
+}
+
+func sumOfDigits(n int) int {
+	// Base case
+	if n < 10 {
+		return n
+	}
+
+	return n%10 + sumOfDigits(n/10)
 }
