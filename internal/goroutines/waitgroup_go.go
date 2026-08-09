44be72dfbb9277e06900a1a9d3e8b5ee44cf45dc
William Munoz <william@rustdeveloper.io>
2026-08-09 00:11:10 -0500

    add waitgroup.go

--- internal/goroutines/waitgroup_go.go
+++ internal/goroutines/waitgroup_go.go
@@ -5,6 +5,12 @@ import (
 	"sync"
 )
 
+func processTask(task string) func() {
+	return func() {
+		fmt.Println("Processing:", task)
+	}
+}
+
 func WaitGroupGo(show bool) {
 	if show {
 		var wg sync.WaitGroup
@@ -12,10 +18,10 @@ func WaitGroupGo(show bool) {
 		tasks := []string{"task1", "task2", "task3", "task4", "task5", "task6", "task7"}
 
 		for _, task := range tasks {
-			wg.Go(func() {
-				fmt.Println("Processing:", task)
-			})
+			// wg.Go handles wg.Add(1) and defer wg.Done() internally!
+			wg.Go(processTask(task))
 		}
+
 		wg.Wait()
 		fmt.Println("--- WaitGroup.Go End ---")
 	}
